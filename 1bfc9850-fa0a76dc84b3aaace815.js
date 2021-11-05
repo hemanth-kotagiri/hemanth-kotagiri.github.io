f8ce6bc816fef691dbb41a2282c998b3273dc427
Hemanth <hemanth.kotagiri43@gmail.com>
2021-11-06 00:45:36 +0530

    Updates

---
+++ 1bfc9850-fa0a76dc84b3aaace815.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{ma3e:function(t,n,a){"use strict";a.d(n,"a",(function(){return e}));var c=a("Lnxd");function e(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 320 512"},child:[{tag:"path",attr:{d:"M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"}}]})(t)}}}]);
+//# sourceMappingURL=1bfc9850-fa0a76dc84b3aaace815.js.map
