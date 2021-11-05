63b18f71c27adee6644b6799d838b53e55c5f570
Hemanth <hemanth.kotagiri43@gmail.com>
2021-11-05 13:34:29 +0530

    New portfolio initial commit

---
+++ src/components/section-footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../css/index.css'
+
+const SectionFooter = () => {
+    return (
+      <div>
+        <hr /><br />
+      <a href="/old" className="footer">For Precious, With Patience</a>
+      </div>
+    );
+}
+
+export default SectionFooter;
